09a10f82ff8db6be1b8df0916f592c7eb412364e
arun3894 <arunchoudhary837@gmail.com>
2024-10-17 15:37:55 +0530

    testAgainCommit

---
+++ HibernateMavenProject/src/main/java/com/jtc/hibernate/App.java
@@ -0,0 +1,13 @@
+package com.jtc.hibernate;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
